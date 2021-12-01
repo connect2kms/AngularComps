3c15407f3737d3e8ba7fe974343b3a5d7c0980cf
Karthik Mohanasundaram <connect2kms@outlook.com>
2021-11-30 19:24:55 -0500

    Child component added for collections

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -6,9 +6,12 @@ import { CollectionsHomeComponent } from './collections-home/collections-home.co
 import { TableComponent } from './table/table.component';
 
 import { SharedModule } from '../shared/shared.module';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 @NgModule({
-  declarations: [CollectionsHomeComponent, TableComponent],
+  declarations: [CollectionsHomeComponent, TableComponent, BiographyComponent, CompaniesComponent, PartnersComponent],
   imports: [CommonModule, CollectionsRoutingModule, SharedModule],
   exports: [],
 })
